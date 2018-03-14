f16fce1f48a5a0b2188da78b9ee5f6ae8d7ff5e8
Jonatas Emidio <jonatasemidio@gmail.com>
2018-03-14 16:10:23 -0300

    Update AppTest.java

--- samples/SpockJava/src/test/java/com/spockjava/AppTest.java
+++ samples/SpockJava/src/test/java/com/spockjava/AppTest.java
@@ -3,7 +3,7 @@ package com.spockjava;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
-
+import static org.junit.Assert.*;
 /**
  * Unit test for simple App.
  */
